0e7dc0422cce721fde5c214e9150a52cd626ad7c
Axhel1 <axelcomoaxelm888@gmail.com>
2024-09-11 18:19:23 +0200

    dia 1

---
+++ Assets/Scripts/Ejercicio1_2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string nombre = "Axel";
+
+        Debug.Log("¡Hola Mundo!");
+        Debug.Log("Este es el primer videojuego de  " + nombre );
+        Debug.Log("Estoy aprendiendo C#  ");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Ha pasado un fram  ");
+    }
+}
