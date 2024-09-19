7d1668eb8488b59974d87ac6b1a835e8c909a41c
Axhel1 <axelcomoaxelm888@gmail.com>
2024-09-19 19:13:38 +0200

    OOP

---
+++ Assets/Scripts/Personaje.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+
+    //Como se define
+    //Propiedades o atributos
+    float vida;
+    float dahio;
+    float exp;
+    string nombre;
+
+
+    //Que puede hacer el personaje
+    //Funcionalidades
+    void Mover()
+    {
+
+
+
+
+    }
+
+    void Atacar() { 
+    
+    
+    
+    
+    }
+
+
+    void Saltar () { 
+    
+    }  
+    
+}
